9e91bc70f83565dd07a6d685d99c1953bc34c89d
Isaac <isaacfiuzavieira@gmail.com>
2026-06-13 12:17:21 -0300

    fix: cria test/system e corrige lint de arrays nas migrations

--- db/migrate/20260613000003_create_contexts.rb
+++ db/migrate/20260613000003_create_contexts.rb
@@ -6,6 +6,6 @@ class CreateContexts < ActiveRecord::Migration[8.1]
       t.timestamps
     end
 
-    add_index :contexts, [:user_id, :name], unique: true
+    add_index :contexts, [ :user_id, :name ], unique: true
   end
 end
